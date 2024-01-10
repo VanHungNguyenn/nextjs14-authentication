01dccb4541851d08ef378759daa5131fd8a5d65f
VanHungNguyen <33939159+VanHungNguyenn@users.noreply.github.com>
2024-01-10 17:07:05 +0700

    middleware and login

--- actions/login.ts
+++ actions/login.ts
@@ -1,6 +1,9 @@
 'use server'
 import * as z from 'zod'
 import { LoginSchema } from '@/schemas'
+import { signIn } from '@/auth'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
+import { AuthError } from 'next-auth'
 
 const login = async (values: z.infer<typeof LoginSchema>) => {
 	const validatedFields = LoginSchema.safeParse(values)
@@ -9,7 +12,28 @@ const login = async (values: z.infer<typeof LoginSchema>) => {
 		return { error: 'Invalid fields' }
 	}
 
-	return { success: 'Email sent!' }
+	const { email, password } = validatedFields.data
+
+	try {
+		await signIn('credentials', {
+			email,
+			password,
+			redirectTo: DEFAULT_LOGIN_REDIRECT,
+		})
+	} catch (error) {
+		if (error instanceof AuthError) {
+			switch (error.type) {
+				case 'CredentialsSignin':
+					return { error: 'Invalid credentials' }
+				case 'EmailSignInError':
+					return { error: 'Invalid email' }
+				default:
+					return { error: 'Something went wrong' }
+			}
+		}
+
+		throw error
+	}
 }
 
 export default login
